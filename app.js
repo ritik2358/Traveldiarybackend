d69a4c75bf881a01549ee4d0c7cd30acf6943bef
Ritik Raj <ritik2358@gmail.com>
2023-05-31 15:41:05 +0530

    added new react app frontend version

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use(express.static(path.join('public')));
 
 app.use((req, res, next) => {
-
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization')
@@ -29,10 +29,15 @@ app.use((req, res, next) => {
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
+
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route.', 404);
-  throw error;
-});
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+// app.use((req, res, next) => {
+//   const error = new HttpError('Could not find this route.', 404);
+//   throw error;
+// });
 
 app.use((error, req, res, next) => {
   if (req.file) {
